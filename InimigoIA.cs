651cd7d32dbf597b111fce66e9477b6f3d82ef42
NyloOly <0001269698@senaimgaluno.com.br>
2026-07-02 15:27:58 -0300

    adiciona logica de movimento

---
+++ InimigoIA.cs
@@ -0,0 +1,16 @@
+using system;
+
+class InimigoIA
+{
+
+    public float Velocidade { get; set; } = 2.0f;
+    public float PosicaoX { get; private set; } = 0.0f;
+
+    public void Mover(float tempo)
+    {
+
+PosicaoX += Velocidade * tempo;
+Console.WriteLine($"Inimigo se moveu para aposição: {PosicaoX}");
+}
+
+}
